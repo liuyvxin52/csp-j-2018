c8dd2211cb018d84bfdc385e6eea14d52b85d7f0
liuyvxin52 <xinxintpai@outlook.com>
2023-08-14 21:22:01 +0800

    modified:   3-3 	modified:   3-3.cpp

--- 3-3.cpp
+++ 3-3.cpp
@@ -1,15 +1,17 @@
 //3.
-#include <iostream>
+#include<iostream>
 using namespace std;
-int n, m;
-int findans(int n, int m) {
-    if (n == 0) return m;
-    if (m == 0) return n % 3;
-    return findans(n - 1, m) - findans(n, m - 1) + findans(n - 1, m - 1);
+int n,m;
+int findans(int n,int m)
+{
+    if(n==0) return m;
+    if(m==0) return n%3;
+    return findans(n-1,m)-findans(n,m-1)+findans(n-1,m-1);
 }
-int main(){
-    cin >> n >> m;
-    cout << findans(n, m) << endl;
+int main()
+{
+    cin>>n>>m;
+    cout<<findans(n,m)<<endl;
     return 0;
 }
 /*
