c9a67e56cda3090378bf865575e14af6c148c683
liuyvxin52 <xinxintpai@outlook.com>
2023-08-14 21:25:07 +0800

    modified:   3-4 	modified:   3-4.cpp

--- 3-4.cpp
+++ 3-4.cpp
@@ -1,24 +1,29 @@
 //4.
-#include <stdio.h>
-int n, d[100];
+#include<stdio.h>
+int n,d[100];
 bool v[100];
 
-int main() {
-    scanf("%d", &n);
-    for (int i = 0; i < n; ++i) {
-        scanf("%d", d + i);
-        v[i] = false;
+int main()
+{
+    scanf("%d",&n);
+    for(int i=0;i<n;++i)
+    {
+        scanf("%d",d+i);
+        v[i]=false;
     }
-    int cnt = 0;
-    for (int i = 0; i < n; ++i) {
-        if (!v[i]) {
-        	for (int j = i; !v[j]; j = d[j]) {
-        		v[j] = true;
+    int cnt=0;
+    for (int i=0;i<n;++i)
+    {
+        if(!v[i])
+        {
+        	for(int j=i;!v[j];j=d[j])
+            {
+        		v[j]=true;
         	}
         	++cnt;
         }
     }
-    printf("%d\n", cnt);
+    printf("%d\n",cnt);
     return 0;
 }
 /*
